f06f055e094d1912ab3672cac4bd78cf62b78d00
James <james@eli.cx>
2023-04-24 01:57:03 +0100

    fix(dynamic-static): return 404 for non-generated params

--- dynamic-static-app-dir-13.2.4/app/dynamic/[slug]/page.tsx
+++ dynamic-static-app-dir-13.2.4/app/dynamic/[slug]/page.tsx
@@ -13,3 +13,7 @@ export const generateStaticParams = async (): Promise<{ slug: string }[]> => {
 	const slugs = ['foo', 'bar', 'baz'];
 	return slugs.map(slug => ({ slug }));
 };
+
+// NOTE: It seems that there is a limitation where it is not possible to generate an edge runtime function for catching dynamic routes non generated at build time.
+// Therefore, we specify that routes not generated at build time can 404.
+export const dynamicParams = false;
