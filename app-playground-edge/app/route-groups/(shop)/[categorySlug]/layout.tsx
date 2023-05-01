ac22de40bb198eda2735d6a46c9b5cb01e8e5325
James <james@eli.cx>
2023-05-01 21:59:43 +0100

    add back route groups and others to app-playground-edge

---
+++ app-playground-edge/app/route-groups/(shop)/[categorySlug]/layout.tsx
@@ -0,0 +1,39 @@
+import { fetchCategoryBySlug } from '../../../../lib/get-categories';
+import { ClickCounter } from '../../../../ui/click-counter';
+import { TabGroup } from '../../../../ui/tab-group';
+import { notFound } from 'next/navigation';
+
+export default async function Layout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { categorySlug: string };
+}) {
+  const category = await fetchCategoryBySlug(params.categorySlug);
+  if (!category) notFound();
+
+  return (
+    <div className="space-y-9">
+      <div className="flex justify-between">
+        <TabGroup
+          path={`/route-groups/${category.slug}`}
+          items={[
+            {
+              text: 'All',
+            },
+            ...category.items.map((x) => ({
+              text: x.name,
+              slug: x.slug,
+            })),
+          ]}
+        />
+
+        <div className="self-start">
+          <ClickCounter />
+        </div>
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+}
