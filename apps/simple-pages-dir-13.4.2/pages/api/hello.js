a04ac505a506a38f0de5263f80637ea5dfc75cd9
Dario Piotrowicz <dario@cloudflare.com>
2023-05-21 18:45:33 +0100

    add node modules usages in simple-pages-dir-13.4.2

--- apps/simple-pages-dir-13.4.2/pages/api/hello.js
+++ apps/simple-pages-dir-13.4.2/pages/api/hello.js
@@ -1,4 +1,9 @@
 import { EventEmitter } from 'node:events';
+import { AssertionError, strict as assert } from "node:assert";
+import { types } from 'node:util';
+import { AsyncLocalStorage } from 'node:async_hooks';
+// import { join } from 'node:path'; <--- not allowed by vercel/next itself
+// import { StringDecoder } from 'node:string_decoder'; <--- not allowed by vercel/next itself
 
 export const runtime = 'edge';
 
@@ -7,5 +12,38 @@ export default function handler() {
   eventEmitter.addListener('randomEvent', () => {});
   eventEmitter.addListener('anotherRandomEvent', () => {});
   const eventNames = eventEmitter.eventNames();
-  return new Response(`hello! eventNames: ${JSON.stringify(eventNames)}`);
+
+  let assertStr = "ERROR: ASSERT ISN'T WORKING PROPERLY!";
+  try {
+    assert(null);
+  } catch (e) {
+    const isE = e instanceof AssertionError;
+    if(isE) {
+      assertStr = 'assert(null) correctly throws an assertion error';
+    }
+  }
+
+  const myAsyncLocalStorage = new AsyncLocalStorage();
+
+  return new Response(
+    `
+    ===== node:events
+
+      eventNames: ${JSON.stringify(eventNames)}
+
+    ===== node:assert
+
+      ${assertStr}
+
+    ===== node:utils
+
+      types.isAsyncFunction(() => {}) === ${types.isAsyncFunction(() => {})}
+
+      types.isAsyncFunction(async () => {}) === ${types.isAsyncFunction(async () => {})}
+
+    ===== node:async_hooks
+
+      typeof myAsyncLocalStorage.run === ${typeof myAsyncLocalStorage.run}
+
+  `);
 }
