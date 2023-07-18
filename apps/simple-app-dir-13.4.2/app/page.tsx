61686a5432267fee8bb3c615cf898052f7bf48be
Dario Piotrowicz <dario@cloudflare.com>
2023-07-18 16:31:31 +0100

    add missing link to simple-app-dir-13.4.2

--- apps/simple-app-dir-13.4.2/app/page.tsx
+++ apps/simple-app-dir-13.4.2/app/page.tsx
@@ -38,6 +38,20 @@ export default function Home() {
           </p>
         </Link>
         <Link
+          href="/api/inline-assets"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Inline assets{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Showcase how we handle inline/bundled assets
+          </p>
+        </Link>
+        <Link
           href="/api/gh-nop"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
