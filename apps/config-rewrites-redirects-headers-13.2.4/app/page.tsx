a598af07e602a6e94a9e04bfec7d589d48606537
James <james@eli.cx>
2023-07-22 21:10:49 +0100

    update config rewrites index page

--- apps/config-rewrites-redirects-headers-13.2.4/app/page.tsx
+++ apps/config-rewrites-redirects-headers-13.2.4/app/page.tsx
@@ -1,27 +1,46 @@
-import { Inter } from "next/font/google";
-import styles from "./page.module.css";
-import Link from "next/link";
+import { Inter } from 'next/font/google';
+import styles from './page.module.css';
+import Link from 'next/link';
 
 export const config = {
-  runtime: "edge",
+  runtime: 'edge',
 };
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const links: { href: string; description: string }[] = [
-    { href: "some-page", description: "Should go to /some-page" },
-    { href: "some-page?overrideMe", description: "Should rewrite to /somewhere-else" },
-    { href: "non-existent", description: "Should rewrite to /contact" },
-    { href: "external", description: "Should rewrite to https://my-old-site.com/external-non-existent" },
-    { href: "about", description: "Should redirect to /info (with status 307)" },
-    { href: "about-permanent", description: "Should redirect to /info (with status 308)" },
+    { href: 'some-page', description: 'Should go to /some-page' },
+    {
+      href: 'some-page?overrideMe',
+      description: 'Should rewrite to /somewhere-else',
+    },
+    { href: 'non-existent', description: 'Should rewrite to /contact' },
+    {
+      href: 'plain',
+      description: 'Should rewrite to https://uuid.rocks/plain',
+    },
+    {
+      href: 'invalid',
+      description: 'Should rewrite to https://uuid.rocks/invalid',
+    },
+    {
+      href: 'about',
+      description: 'Should redirect to /info (with status 307)',
+    },
+    {
+      href: 'about-permanent',
+      description: 'Should redirect to /info (with status 308)',
+    },
   ];
 
   return (
     <main className={styles.main}>
       <div className={styles.center}>
-        <h2>The response for this page (/) should include a &quot;x-hello&quot; header with value &quot;world&quot;</h2>
+        <h2>
+          The response for this page (/) should include a &quot;x-hello&quot;
+          header with value &quot;world&quot;
+        </h2>
       </div>
 
       <div className={styles.grid}>
